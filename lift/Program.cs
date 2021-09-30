75140157c257c1f7c64ca5822bf524a7ae2d6d70
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-30 10:45:41 +0200

    Add files via upload

---
+++ lift/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, n, a, b;
+
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+
+            a = n / k;
+            b = n % k;
+           
+            if (b > 0)
+            {
+                a++;
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
